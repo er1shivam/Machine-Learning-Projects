a327e37b01d104b2df006ccf6dbdae1803d9fa2b
er1shivam <er1shivam@gmail.com>
2018-01-02 23:24:40 +0530

    Added function to load data

--- fetch_dataset.py
+++ fetch_dataset.py
@@ -7,6 +7,9 @@ HOUSING_PATH = "datasets/housing"
 HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + "/housing.tgz"
 
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    # Function to fetch the dataset from given url
+    # Just run once to get the data
+
     if not os.path.isdir(housing_path):
         os.makedirs(housing_path)
         
@@ -16,3 +19,8 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz = tarfile.open(tgz_path)
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
+
+
+def load_housing_data(housing_path =HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
